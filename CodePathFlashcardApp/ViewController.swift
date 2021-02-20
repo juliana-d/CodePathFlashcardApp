4afd2390ebab9a9d5e44d38f57c1653bd8063510
Juliana Dudziak <juliana.dudziak@gmail.com>
2021-02-20 15:35:47 -0500

    Initial Commit

--- CodePathFlashcardApp/ViewController.swift
+++ CodePathFlashcardApp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        questionLabel.isHidden = true
+    }
+    
 }
 
